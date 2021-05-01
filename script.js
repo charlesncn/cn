df7c315ecae9b71f2336f56ae36d8c26dedd9b19
charlesncn <53397655+charlesncn@users.noreply.github.com>
2021-05-01 13:50:33 +0300

    updated

---
+++ script.js
@@ -0,0 +1,37 @@
+$(document).ready(function(){
+    $(window).scroll(function(){
+        if(this.scrollY > 30){
+            $('.navBar').addClass("sticky");
+        }
+        else{
+            $('.navBar').removeClass("sticky");
+        }
+    });
+    // menu icon
+    $('.menu_btn').click(function(){
+        $('.navBar .menu').toggleClass("active");
+        $('.menu_btn i').toggleClass("active");
+    });
+
+    // carousel
+    $('.carousel').owlCarousel({
+        margin: 20,
+        loop: true,
+        autoplayTimeOut: 2000,
+        autoplayHoverPause: true,
+        responsive: {
+            0:{
+                items: 1,
+                nav: false
+            },
+            600:{
+                items: 2,
+                nav: false
+            },
+            1000:{
+                items: 3,
+                nav: false
+            }
+        }
+    });
+});
